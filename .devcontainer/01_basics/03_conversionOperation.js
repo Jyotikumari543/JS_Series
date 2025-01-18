34e344216b03133c1dfddce6386e9c126a1daaa8
Jyoti kumari <113599566+Jyotikumari543@users.noreply.github.com>
2025-01-18 05:39:30 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score=33;
+// let score="33abc";
+//let score=null
+//let score=undefined
+// let score="33";//suppose we are making score as string write the number under " "
+// console.log(typeof score);//so it will print number . then it give string
+// console.log(typeof (score));//same it will also print number .same as above it will also give string
+
+// let valueInNumber=Number(score)//it will convert the string in number datatype  
+// console.log(typeof valueInNumber);//but if it written in mix  character and number like 33abc it will show datatype as number
+// console.log(valueInNumber)//while printing i will give NaN
+
+//"33"=>33
+//"33abc"=>NaN but type give as number
+//true=>1;false=>0
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+//1=>true;
+// 0=>false 
+// jyoti=true
+//""=false
+
+let someNumber=22
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
